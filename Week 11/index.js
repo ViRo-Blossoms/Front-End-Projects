65b83bb2b2bbb5c30b32d55ea8f182d0579ae36f
ViRo-Blossoms <viroblossoms@gmail.com>
2024-12-19 22:30:25 -0500

    Week 11 - Code Cleanup

--- Week 11/index.js
+++ Week 11/index.js
@@ -6,29 +6,11 @@
 // if all squares are disabled and none are the 10 possible combos, all lose
 // if one of the combos is complete, that player wins
 
-// alt:
-// buttons start disabled 
-// players enter names
-// renable buttons
-// computer picks player to go first
-// player take turns clicking squares to take them
-// set the value of that square and disable it
-// if all squares are disabled and none are the 10 possible combos, all lose
-// if one of the combos is complete, that player wins
-
-// Stretch:
-// PLayer vs Cpu
-// flip to see who goes first
-// click squares to claim them
-// add unclaimed squares to list, sort randomly, pop to have cpu pick
-// set the value of that square and disable it
-// if all squares are disabled and none are the 10 possible combos, all lose
-// if one of the combos is complete, that player wins
-
-// use setAtribute('class', );?
-//use styleresult?
-// classList.add/remove
-//vercel - use to deply websites
+//BUG
+//The first round goes fine, but resetting the board makes it think it's always player 2's turn 
+//The console suggests that its registering two clicks or maybe there's two buttons? We basically wanted
+//to show we could do it with minimal tutorials, so if friday we can't make it work we'll just watch the 
+//promineo one lmao
 
 //Init Setup
 var player1 = '';
@@ -40,6 +22,8 @@ var posLong = '';
 var p1Score = 0;
 var p2Score = 0;
 
+var bibbidi = ['TL', 'TM', 'TR', 'ML', 'MM', 'MR', 'BL', 'BM', 'BR']
+
 var sTL = '';
 var sTM = '';
 var sTR = '';
@@ -50,17 +34,22 @@ var sBL = '';
 var sBM = '';
 var sBR = '';
 
-// PVP CODE
+
+// Start CODE
 document.getElementById('PvP').addEventListener('click', () =>{
-    gameActive = 1;
-    let player1 = document.getElementById('p1Name').value;
-    let player2 = document.getElementById('p2Name').value;
+    gameActive = 1; //Doesn't do anything atm
+
+    //Set player names
+    player1 = document.getElementById('p1Name').value;
+    player2 = document.getElementById('p2Name').value;
     if (player1 == '') {
         player1 = 'Jeremy Francois';
     };
     if (player2 == '') {
         player2 = 'Dell Conagher';
     };
+
+    //Activate board
     document.getElementById('TL').classList.remove('disabled')
     document.getElementById('TM').classList.remove('disabled')
     document.getElementById('TR').classList.remove('disabled')
@@ -73,69 +62,61 @@ document.getElementById('PvP').addEventListener('click', () =>{
     document.getElementById('PvP').classList.add('disabled')
     document.getElementById('Reset').classList.remove('disabled')
 
-
-        document.getElementById('Reset').addEventListener('click', () => {
+    document.getElementById('Reset').addEventListener('click', () => {
             reset();
         })
 
-            document.getElementById('TL').addEventListener('click', () =>{
-                let posShort = 'TL';
-                let posLong  = 'Top Left';
-                // if (turn % 2 == 0) {
-                //     document.getElementById('TL').classList.add('btn-primary');
-                //     document.getElementById('TL').value = 'X';
-                //     document.getElementById('littleGreen').innerHTML = `${player1} takes top left.`
-                // } else {
-                //     document.getElementById('TL').classList.add('btn-danger');
-                //     document.getElementById('TL').value = 'O';
-                //     document.getElementById('littleGreen').innerHTML = `${player2} takes ${position}.`
-                // };
-                determine(posShort, posLong, player1, player2);
-            });
-            document.getElementById('TM').addEventListener('click', () =>{
-                let posShort = 'TM';
-                let posLong = 'Top Middle';
-                determine(posShort, posLong, player1, player2);
-            })
-            document.getElementById('TR').addEventListener('click', () =>{
-                let posShort = 'TR';
-                let posLong = 'Top Right';
-                determine(posShort, posLong, player1, player2);
-            })
-            document.getElementById('ML').addEventListener('click', () =>{
-                let posShort = 'ML';
-                let posLong = 'Middle Left';
-                determine(posShort, posLong, player1, player2);
-            })
-            document.getElementById('MM').addEventListener('click', () =>{
-                let posShort = 'MM';
-                let posLong = 'Middle Middle';
-                determine(posShort, posLong, player1, player2);
-            })
-            document.getElementById('MR').addEventListener('click', () =>{
-                let posShort = 'MR';
-                let posLong = 'Middle Right';
-                determine(posShort, posLong, player1, player2);
-            })
-            document.getElementById('BL').addEventListener('click', () =>{
-                let posShort = 'BL';
-                let posLong = 'Bottom Left';
-                determine(posShort, posLong, player1, player2);
-            })
-            document.getElementById('BM').addEventListener('click', () =>{
-                let posShort = 'BM';
-                let posLong = 'Bottom Middle';
-                determine(posShort, posLong, player1, player2);
-            })
-            document.getElementById('BR').addEventListener('click', () =>{
-                let posShort = 'BR';
-                let posLong = 'Bottom Right';
-                determine(posShort, posLong, player1, player2);
-            })
+        //We think the bug lies in here somewhere, it's probably definetly from all the buttons getting a new listener
+
+        document.getElementById('TL').addEventListener('click', () =>{
+            let posShort = 'TL';
+            let posLong  = 'Top Left';
+            determine(posShort, posLong);
+        });
+        document.getElementById('TM').addEventListener('click', () =>{
+            let posShort = 'TM';
+            let posLong = 'Top Middle';
+            determine(posShort, posLong);
         })
+        document.getElementById('TR').addEventListener('click', () =>{
+            let posShort = 'TR';
+            let posLong = 'Top Right';
+            determine(posShort, posLong);
+        })
+        document.getElementById('ML').addEventListener('click', () =>{
+            let posShort = 'ML';
+            let posLong = 'Middle Left';
+            determine(posShort, posLong);
+        })
+        document.getElementById('MM').addEventListener('click', () =>{
+            let posShort = 'MM';
+            let posLong = 'Middle Middle';
+            determine(posShort, posLong);
+        })
+        document.getElementById('MR').addEventListener('click', () =>{
+            let posShort = 'MR';
+            let posLong = 'Middle Right';
+            determine(posShort, posLong);
+        })
+        document.getElementById('BL').addEventListener('click', () =>{
+            let posShort = 'BL';
+            let posLong = 'Bottom Left';
+            determine(posShort, posLong);
+        })
+        document.getElementById('BM').addEventListener('click', () =>{
+            let posShort = 'BM';
+            let posLong = 'Bottom Middle';
+            determine(posShort, posLong);
+        })
+        document.getElementById('BR').addEventListener('click', () =>{
+            let posShort = 'BR';
+            let posLong = 'Bottom Right';
+            determine(posShort, posLong);
+        })
+    })
 
 // FUNC
-    function determine (posShort, posLong, player1, player2) {
+    function determine (posShort, posLong) {
         document.getElementById(posShort).classList.remove('btn-secondary');
         document.getElementById(posShort).classList.add('disabled');
         if (turn % 2 == 0) { //PLAYER 1 - Blue X
@@ -231,19 +212,40 @@ document.getElementById('PvP').addEventListener('click', () =>{
         }
         else if (sBL + sMM + sTR == 'OOO') {
             declare(false)
-        }  
+        }
+
+        //Stalemate
+        else if (turn == 8)
+            loser();  
     };
 
+    function loser () {
+        document.getElementById('bigGreen').innerHTML =`You both lose!`
+        document.getElementById('littleGreen').innerHTML = `${player2} and ${player1}, wanna try again?`
+        console.log(`${player1} wins`)
+    }
+
     function declare (winner) {
+        lockdown();
         if (winner == true) { //P1
-
+            document.getElementById('bigGreen').innerHTML =`${player1} Wins in ${turn} turns!`
+            document.getElementById('littleGreen').innerHTML = `Try again for a Zeni, ${player2}?`
+            console.log(`${player1} wins`)
         } else { //P2
-
+            document.getElementById('bigGreen').innerHTML =`${player2} Wins in ${turn} turns!`
+            document.getElementById('littleGreen').innerHTML = `Try again for a Zeni, ${player1}?`
+            console.log(`${player2} wins`)
         }
     }
 
+    function lockdown () {
+        bibbidi.forEach(element => //Disable buttons
+            document.getElementById(element).classList.add('disabled')
+        )
+    }
+
     function reset () {
-     let bibbidi = ['TL', 'TM', 'TR', 'ML', 'MM', 'MR', 'BL', 'BM', 'BR']
+     
         turn = 0;
         gameActive = 0;
 
@@ -258,10 +260,6 @@ document.getElementById('PvP').addEventListener('click', () =>{
         bibbidi.forEach(element => //Reset button color
             document.getElementById(element).classList.add('btn-secondary')
         );
-
-        bibbidi.forEach(element => //Disable buttons
-            document.getElementById(element).classList.add('disabled')
-        )
 
         bibbidi.forEach(element =>  //Reset button value
             document.getElementById(element).value = ''
