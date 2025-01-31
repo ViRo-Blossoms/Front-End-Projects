d0799618917b3d01a9a20a970c62ecb4e0ddf5df
ViRo-Blossoms <viroblossoms@gmail.com>
2025-01-31 12:13:47 -0500

    Image fix for Week 14, thank you Romaric!

--- Week 14/src/Post.tsx
+++ Week 14/src/Post.tsx
@@ -14,9 +14,14 @@ export default function Post({
     datetime: number;
     user: string;
     postBody: string;
-    // image: string;
+    image?: string;
   };
 }) {
+  // Huge thank you to Romaric Z for helping us debug this,
+  // it turned out that we had simply messed up the variable names.
+  // So the post prop wanted an 'image' string in Posts and an 'img' string in PostFeed. Oops!
+
+  console.log(post);
   return (
     <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
       <p>
@@ -24,7 +29,7 @@ export default function Post({
       </p>
       <p>{post.datetime}</p>
       <p>{post.postBody}</p>
-      {/* <img src={post.image} alt="" /> */}
+      <img src={post.image} alt="" />
     </div>
   );
 }
--- Week 14/src/PostFeed.tsx
+++ Week 14/src/PostFeed.tsx
@@ -5,31 +5,35 @@ import Post from "./Post";
 //User - string*
 //Post Body - String
 //Image - String (path) Optional?
-// import Uchigatana from "./assets/Uchigatana.jpg";
-// import pumpkin from "./assets/pumpkin.jpg";
+import Uchigatana from "./assets/Uchigatana.jpg";
+import pumpkin from "./assets/pumpkin.svg";
 const testPosts = [
   {
     id: 1,
     datetime: 1737815428006,
     user: `ViRo`,
     postBody: `Welcome to the forum for UM! This site is all about the new hit game Uchigatana Martial. Please read the rules before posting, and remember: 'Go For It!!'`,
-    // img: Uchigatana,
+    image: Uchigatana,
   },
   {
     id: 2,
     datetime: 1737817342450,
     user: `BursterSpeed`,
     postBody: "first",
-    // img: Uchigatana,
+    image: Uchigatana,
   },
   {
     id: 14,
     datetime: 1730311218647,
     user: `Grandma`,
     postBody: "Look at this lovely pie I just made! :)",
-    // img: pumpkin,
+    image: pumpkin,
   },
 ];
+
+// Huge thank you to Romaric Z for helping us debug this,
+// it turned out that we had simply messed up the variable names.
+// So the post prop wanted an 'image' string in Posts and an 'img' string in PostFeed. Oops!
 
 //Thing that holds the posts
 export default function PostFeed() {
